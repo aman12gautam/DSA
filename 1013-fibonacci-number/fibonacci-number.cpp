8e012371baeed31cb2862f136e48f722089b0705
Aman Gautam <amangautam9897.com@gmail.com>
2026-09-11 22:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (38.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,7 +7,7 @@ public:
             return 1;
         }
         int firstterm = 0, secondterm=1;
-        for(int i=0; i<n; i++){
+        for(int i=1; i<=n; i++){
             int thirdterm = firstterm+secondterm;
 
             firstterm = secondterm;
